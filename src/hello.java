44682f0cadf92ed6de8f299ef79ee4b1c6e0b314
Pavilion <Pavilion>
2016-08-19 20:55:55 +0500

    first commit

---
+++ src/hello.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class hello {
+	 public static void main(String[] b) {
+
+         System.out.println("Hello!");
+
+         apple s1 = new apple();
+         s1.input();
+         s1.random(s1.getFirstNumber() , s1.getSecondNumber());
+
+}
+
+}
